137c35996bb78d1aebeba12b3f0f0c5cff82af6d
accio <accio@Prikshits-MacBook-Air.local>
2023-11-13 21:36:48 +0530

    filter polyfill

---
+++ 6th Class/filter-polyfill.js
@@ -0,0 +1,27 @@
+delete Array.prototype.filter;
+
+Array.prototype.filter = function (callBack) {
+    const result = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callBack(this[i], i)) {
+            result.push(this[i]);
+        }
+    }
+    return result;
+}
+
+// let age = [20, 17, 18, 14];
+
+// let newAge = age.filter((item) => {
+//     // if (item < 18) return false;
+//     // else return true;
+//     return item >= 18;
+// });
+
+// console.log(newAge);
+
+
+let oldArr = [false, false, false];
+let newArr = oldArr.filter(item => item);
+
+console.log(newArr);
